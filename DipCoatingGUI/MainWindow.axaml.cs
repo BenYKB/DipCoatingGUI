b88b7f6bfee8a21fefd66df4d9c54dee21bd96af
BenYKB <benjaminykb@gmail.com>
2020-10-19 00:09:46 -0600

    initial commit

---
+++ DipCoatingGUI/MainWindow.axaml.cs
@@ -0,0 +1,69 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using DipCoatingGUI.Models;
+using Phidget22;
+using Phidget22.Events;
+using System;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace DipCoatingGUI
+{
+    public class MainWindow : Window
+    {
+        const int MIN_PULSEWIDTH = 800000;
+        const int MAX_PULSEWIDTH = 2000000;
+        const double POSITION_AT_MIN_PULSEWIDTH = 0;
+        const double POSITION_AT_MAX_PULSEWIDTH = 180;
+        private RCServo servo;
+        private Stopwatch stopWatch;
+        private DispatcherTimer timer;
+        private int ticks;
+        public NumericUpDown NumberOfCycles;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ControllerViewModel();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        public void onUpButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.Content = $"{ticks}";
+        }
+
+        public void onDownButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.Content = $"{ticks}";
+        }
+
+       public void onConnectClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            button.Content = "Connected";
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            NumberOfCycles = new NumericUpDown();
+            ticks = 0;
+            timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, onTick);
+            timer.Start();
+            
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+            ticks += 1;
+        }
+    }
+}
---
+++ DipCoatingGUI/Models/ControllerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DipCoatingGUI.Models
+{
+    class ControllerViewModel : INotifyPropertyChanged
+    {
+        private double _numCycles = 5;
+
+        public double NumCycles
+        {
+            get => _numCycles;
+            set
+            {
+                if (Math.Abs(value - _numCycles) > 0.25) {
+                    _numCycles = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
